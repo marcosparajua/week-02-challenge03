38eb65a3b9124ca360c7fa37ac728887d531cb43
marcos parajua <marcosparajua@gmail.com>
2024-03-07 10:23:32 +0100

    replace string guard with error message

---
+++ arrays.js
@@ -0,0 +1,50 @@
+<<<<<<< Updated upstream
+=======
+export const manualArrayLength = (array) => {
+  let r = 0;
+
+  for (const iterator of array) {
+    r++;
+  }
+
+  return r;
+};
+
+export const fakeIndexOf = (arr, arrayElement) => {
+  if (arrayElement === undefined) {
+    throw new Error('you must indicate an element to be indexed.');
+  }
+  let elementIndex = -1;
+  const arrayLength = manualArrayLength(arr);
+  for (let i = 0; i < arrayLength; i++) {
+    if (arrayElement === arr[i]) {
+      elementIndex = i;
+      break;
+    }
+  }
+  return elementIndex;
+};
+
+export const fakePush = (arr, ...p) => {
+  const arrayLength = manualArrayLength(arr);
+  const pLength = manualArrayLength(p);
+  for (let i = 0; i < pLength; i++) {
+    arr[arrayLength + i] = p[i];
+  }
+  const newLength = manualArrayLength(arr);
+  return newLength;
+};
+export const multiply = (a) => {
+  return a * 2;
+};
+
+export const fakeMap = (arr, func) => {
+  const arrayLength = manualArrayLength(arr);
+  let newArray = new Array(arrayLength);
+  for (let i = 0; i < arrayLength; i++) {
+    const element = func(arr[i]);
+    newArray[i] = element;
+  }
+  return newArray;
+};
+>>>>>>> Stashed changes
